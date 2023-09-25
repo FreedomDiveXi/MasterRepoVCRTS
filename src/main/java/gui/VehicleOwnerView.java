2a2e2b283be8654c1330179592d58036f470b830
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-25 11:57:04 -0400

    vehicle owner gui layout done

--- src/main/java/gui/VehicleOwnerView.java
+++ src/main/java/gui/VehicleOwnerView.java
@@ -1,4 +1,28 @@
 package gui;
+
+import javax.swing.*;
+
 //TODO
 public class VehicleOwnerView {
+    private JPanel mainFrame;
+    private JPanel header;
+    private JPanel body;
+    private JPanel footer;
+    private JButton newVechicle;
+    private JPanel innerCenterBody;
+    private JButton activeJobs;
+    private JButton history;
+    private JButton findAJobButton;
+    private JButton exportData;
+    private JLabel headerText;
+    private JLabel headerText2;
+    private JLabel headerText3;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VehicleOwnerView");
+        frame.setContentPane(new VehicleOwnerView().mainFrame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
