b5deccc36909387ce332f2c547d17a06095222b3
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-25 17:34:36 -0400

    refactored logic to use users folder

---
+++ src/main/java/users/vehicleOwner/Vehicle.java
@@ -0,0 +1,32 @@
+package users.vehicleOwner;
+
+public class Vehicle {
+    private String vehicleModel;
+    private String vehicleMake;
+    private int vehicleYear;
+
+    /**
+     * method will create a new vehicle object
+     *
+     * @param vehicleModel
+     * @param vehicleMake
+     * @param vehicleYear
+     */
+    public Vehicle(String vehicleModel, String vehicleMake, int vehicleYear) {
+        this.vehicleModel = vehicleModel;
+        this.vehicleMake = vehicleMake;
+        this.vehicleYear = vehicleYear;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public String getVehicleMake() {
+        return vehicleMake;
+    }
+
+    public int getVehicleYear() {
+        return vehicleYear;
+    }
+}
