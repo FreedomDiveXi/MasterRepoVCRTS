9aed1165cbf43be671f0998f58734c1f3f9c2a71
Trevin Bachan <trevin.bachan19@my.stjohns.edu>
2023-09-26 17:19:34 -0400

    Changes to be committed: 	modified:   src/main/java/Owner.java 	modified:   src/main/java/users/admin/Client.java

--- src/main/java/users/admin/Client.java
+++ src/main/java/users/admin/Client.java
@@ -5,5 +5,41 @@ public class Client {
     private String jobDuration;
     private String jobDeadline;
 
+    // Constructor 
+    public Client(String ownerID, String jobDuration, String jobDeadline) {
+        this.ownerID = ownerID;
+        this.jobDuration = jobDuration;
+        this.jobDeadline = jobDeadline;
+    }
+
+    // Getter for ownerID
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    // Setter for ownerID
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    // Getter for jobDuration
+    public String getJobDuration() {
+        return jobDuration;
+    }
+
+    // Setter for jobDuration
+    public void setJobDuration(String jobDuration) {
+        this.jobDuration = jobDuration;
+    }
+
+    // Getter for jobDeadline
+    public String getJobDeadline() {
+        return jobDeadline;
+    }
+
+    // Setter for jobDeadline
+    public void setJobDeadline(String jobDeadline) {
+        this.jobDeadline = jobDeadline;
+    }
 
 }
