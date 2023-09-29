99b2eccc6e4ba8c27d6f013250c7176daf0a9f1c
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-29 12:50:31 -0400

    moved file to admin folder, removed duplicate client file

---
+++ src/main/java/users/admin/Owner.java
@@ -0,0 +1,38 @@
+package users.admin;
+public class Owner {
+
+    private String ownerID;
+    private String ownerInfo;
+    private String residancyString;
+
+    // Getter for ownerID
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    // Setter for ownerID
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    // Getter for ownerInfo
+    public String getOwnerInfo() {
+        return ownerInfo;
+    }
+
+    // Setter for ownerInfo
+    public void setOwnerInfo(String ownerInfo) {
+        this.ownerInfo = ownerInfo;
+    }
+
+    // Getter for residancyString
+    public String getResidancyString() {
+        return residancyString;
+    }
+
+    // Setter for residancyString
+    public void setResidancyString(String residancyString) {
+        this.residancyString = residancyString;
+    }
+
+}
