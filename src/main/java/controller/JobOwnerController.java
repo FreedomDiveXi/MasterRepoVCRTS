edf0533be112c19a527dd45f5efc289daed2509e
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-29 17:10:57 -0400

    added controllers

---
+++ src/main/java/controller/JobOwnerController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * Our job objects for now live as a singular entities.
+ * The controller is meant to bridge them together.
+ * That way we can separate the data as much as possible from function/behavior things (except for essential things).
+ * So that way we don't have to directly work with the direct object.
+ */
+
+//todo
+public class JobOwnerController {
+}
---
+++ src/main/java/controller/VehicleOwnerController.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * Our vehicle objects for now live as a singular entities.
+ * The controller is meant to bridge them together.
+ * That way we can separate the data as much as possible from function/behavior things (except for essential things).
+ * So that way we don't have to directly work with the direct object.
+ */
+
+//todo
+public class VehicleOwnerController {
+}
+
