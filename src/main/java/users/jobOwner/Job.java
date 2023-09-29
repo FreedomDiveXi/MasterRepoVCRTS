d595e89ce1f76a3cd228e31265910dd473356791
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-29 16:46:39 -0400

    updated job class

--- src/main/java/users/jobOwner/Job.java
+++ src/main/java/users/jobOwner/Job.java
@@ -2,59 +2,57 @@ package users.jobOwner;
 
 import java.time.LocalTime;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Job {
-    private String jobOwnerName;
+
     private int jobID;
     private LocalTime jobDurationTime;
     private LocalDate jobDeadline;
-    private boolean jobCompletion;
+    private boolean completed;
 
     /**
      * if no deadline is provided will use this constructor method
      *
-     * @param jobOwnerName
      * @param jobID
      * @param jobDurationTime
-     * @param jobCompletion
      */
-    public Job(String jobOwnerName, int jobID, LocalTime jobDurationTime, boolean jobCompletion) {
-        this.jobOwnerName = jobOwnerName;
+    public Job( int jobID, LocalTime jobDurationTime) {
+
         this.jobID = jobID;
         this.jobDurationTime = jobDurationTime;
-        jobCompletion = false;
-
+        this.completed = false;
     }
 
     /**
      * overloaded method to provide an optional case in which we can create a
      * job with a deadline
      *
-     * @param jobOwnerName
      * @param jobID
      * @param jobDurationTime
      * @param jobDeadline
-     * @param jobCompletion
      */
-    public Job(String jobOwnerName, int jobID, LocalTime jobDurationTime, LocalDate jobDeadline, boolean jobCompletion) {
-        this.jobOwnerName = jobOwnerName;
+    public Job(int jobID, LocalTime jobDurationTime, LocalDate jobDeadline) {
         this.jobID = jobID;
         this.jobDurationTime = jobDurationTime;
         this.jobDeadline = jobDeadline;
-        jobCompletion = false;
-
+        this.completed = false;
     }
 
     public int getJobID() {
         return jobID;
     }
-
-    public boolean isJobCompletion() {
-        return jobCompletion;
+    public LocalTime getJobDurationTime() {
+        return jobDurationTime;
+    }
+    public LocalDate getJobDeadline() {
+        return jobDeadline;
     }
 
-    public void setJobCompletion(boolean jobCompletion) {
-        this.jobCompletion = jobCompletion;
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
