42b47d80edc7637a6a08e7a036a1fbf750a60c46
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-30 12:11:59 -0400

    added methods

--- src/main/java/controller/JobVehicleInterface.java
+++ src/main/java/controller/JobVehicleInterface.java
@@ -1,13 +1,28 @@
 package controller;
 
+import users.vehicleOwner.VehicleOwner;
+
 import java.util.ArrayList;
 
 public interface JobVehicleInterface<T> {
+
+
+    //method that will create a user
+    public VehicleOwner createUser(String id, String password);
+
+    public ArrayList<T> addToGlobalList (T item);
+
+    public void updateStatus();
+
+    public ArrayList<T> removeItemFromGlobalList(T item);
+
+    public ArrayList<T> updateGlobalList(ArrayList<T> globalList);
+
     //method that will be able to add something their respected entity onto their list
-    public ArrayList<T> addToList(T Item, ArrayList <T> list );
+    public ArrayList<T> addToList(T item, ArrayList <T> list );
 
     //method that will remove item from the list
-    public ArrayList<T> removeFromList(T Item, ArrayList <T> list );
+    public ArrayList<T> removeFromList(T item, ArrayList <T> list );
 
     // method that will print the details from the list,
     // Meant to be served as a way to print out to a file
