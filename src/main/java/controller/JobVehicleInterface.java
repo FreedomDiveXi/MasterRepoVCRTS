7a1ae470a1d4d9f454bdd36e63a6a3ce05fafa0d
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-30 13:03:01 -0400

    simplified interface
    generics are too hard to implement and its easier to just overload a method

--- src/main/java/controller/JobVehicleInterface.java
+++ src/main/java/controller/JobVehicleInterface.java
@@ -1,31 +1,14 @@
 package controller;
 
+import users.vehicleOwner.Vehicle;
 import users.vehicleOwner.VehicleOwner;
 
 import java.util.ArrayList;
 
 public interface JobVehicleInterface<T> {
 
-
-    //method that will create a user
     public VehicleOwner createUser(String id, String password);
 
-    public ArrayList<T> addToGlobalList (T item);
-
-    public void updateStatus();
-
-    public ArrayList<T> removeItemFromGlobalList(T item);
-
-    public ArrayList<T> updateGlobalList(ArrayList<T> globalList);
-
-    //method that will be able to add something their respected entity onto their list
-    public ArrayList<T> addToList(T item, ArrayList <T> list );
-
-    //method that will remove item from the list
-    public ArrayList<T> removeFromList(T item, ArrayList <T> list );
-
-    // method that will print the details from the list,
-    // Meant to be served as a way to print out to a file
-    public void  printDetails(ArrayList<T> list);
+    public void updateStatus(T item);
 
 }
