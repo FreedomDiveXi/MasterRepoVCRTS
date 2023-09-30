f7b03405d793180624136f0d015af79c6b074347
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-09-30 13:56:57 -0400

    reuploaeded gui files
    we dont have time to make them manually!

---
+++ src/main/java/gui/Registration.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javax.swing.*;
+
+public class Registration {
+    private JPanel MainFrame;
+    private JPanel CenterMainFrame;
+    private JTextField UsernameField;
+    private JTextField PasswordField;
+    private JCheckBox CheckboxValue;
+    private JLabel CheckboxLabel;
+    private JPanel QuestionBox;
+    private JLabel UsernameLabel;
+    private JLabel PasswordLabel;
+    private JButton CompletionButton;
+    private JPanel Confirmation;
+
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame("Registration");
+//        frame.setContentPane(new Registration().MainFrame);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
+}
