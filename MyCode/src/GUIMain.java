ab1d08ed4fd988a37e6cbeb8f706748056298bab
Lawrence Lai <144949740+FreedomDiveXi@users.noreply.github.com>
2023-10-05 18:34:19 -0400

    Add files via upload

---
+++ MyCode/src/GUIMain.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+
+public class GUIMain {
+	public static void main (String [] args) throws IOException {
+		JFrame startPage = new StartPage();
+		startPage.setTitle("Welcome to the Vehicular Cloud");
+		startPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		startPage.setVisible(true);
+	}
+}
