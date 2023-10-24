09de831b6ec4cb244d9222d7a77d72244fc678af
TunaMadu <michael.lopez19@my.stjohns.edu>
2023-10-24 10:01:44 -0400

    update controller
    still barebones though have a better idea how things works, some methods needs revisting as its not clear why we have methods that imply the same thing, still waiting on the vehicle and job files to flush things out better

--- src/CloudController.java
+++ src/CloudController.java
@@ -7,9 +7,9 @@ import java.util.Queue;
 public class CloudController {
 
     // this class will be responsible for most of the processing gui logic.
-    // one major assumption is that the controller is not responsible for the creation of either
-    // respected owners. the controller is only given access to the users information via the gui on submission
-    // of jobs and vehicles.
+    // one major assumption is that the controller is not responsible for the 
+    // creation of either respected owners. the controller is only given access
+    // to the users information via method calling
 
     VehicleOwner vehicleOwnerTest;
     JobOwner jobOwnerTest;
@@ -20,12 +20,10 @@ public class CloudController {
     private ArrayList<Vehicle> inUseVehicles = new ArrayList<>();
 
 
-    // this will be replaced to be of type jobs.
     // FIFO data structure to store the newly created jobs as they come in
     private Queue<Job> availableJobs = new LinkedList<Job>();
     private Queue<Job> activeJobs = new LinkedList<Job>();
 
-    // this will be replaced to be of type jobs
     // as the jobs are completed will store them into a simple list
     private ArrayList<Job> completedJobs = new ArrayList<Job>();
 
@@ -66,9 +64,11 @@ public class CloudController {
                 activeJobs.add(aux);
 
             }else{
+                // if its empty simply add it
                 activeJobs.add(activeJobs.remove());
             }
         }
+        // once acitve jobs are updated pop the first job in the list
         // begin the work on the first in line job
         // 20 --40 60--
         // 60 120
@@ -76,13 +76,24 @@ public class CloudController {
         return activeJobs.remove();
     }
 
-    //todo responsible to set the # of cars associated to some job to keep things simple it will randomize a
-    // number between 1-3. anything < 3, only one car will be assigned to each job.
+    // responsible for generating a random number 1-3 iff theres enough 
+    // availableVehicles 
     public int redundancy() {
+
+        if(availableVehicles.isEmpty())
+            return 0;
+
+        if(availableVehicles.size() < 3)
+            return 1;
+
         return (int)Math.floor(Math.random() * (3-1 + 1 ) + 1);
     }
 
-    //todo
+    // todo the method will run inbetween the time it takes to complete the job
+    // it will generate a new redundancy and copy the job onto more vehicles
+    // if we have a delay while executing we can say at the half way mark
+    // when we recruit more vehicle we can shave off 1 sec/hour/whatever
+    // per # of vehicles
     public void updateCheckpoint() {
         // based on the time. 10
         // 10 secs after 5 seconds we run this method
@@ -90,25 +101,37 @@ public class CloudController {
     }
 
     //todo
-    public void assignJobToVehicle() {
+    public void assignJobToVehicle(Job job, Vehicle vehicle) {
+        // the vehicle must be able to register a job onto itself.
+        // when done, the vehicle will be removed from availableVehicles
     }
 
-    //todo ***
-    public void seeAllDataBases() {
-    }
 
-    //todo ***
+    //todo *** may be redudant since only one job will be processed at a time
+    // a method already exists to see what jobs are next up on the list
     public void seeProgressOfJob() {
     }
 
     //todo ***
-    public void eraseVehicleComputation() {
+    public void eraseVehicleComputation(Vehicle vehicle) {
+        // vehicle should be able to return the job assigned to it so we can
+        // both remove it from the vehicle itself, and from the active jobs or
+        // available jobs
     }
 
-    //todo **
-    public void reassignJob() {
+    //todo ** this will remove the job from some active vehicle and will rassign
+    // it to another available vehicle
+    public void reassignJob(Vehicle vehicle) {
     }
 
+
+
+
+
+    // leave till further notice
+    //todo ***
+    public void seeAllDataBases() {
+    }
     //todo **
     public void updateDatabaseToServer() {
     }
