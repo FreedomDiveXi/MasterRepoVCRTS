7642dd6ebe52a5606e99dc4de8daa3b0f30a97b2
Tanzim Miage <99355110+tmiage@users.noreply.github.com>
2023-10-25 14:04:09 -0400

    Create Vehicle.java Class
    Uploaded Vehicle.java Class

---
+++ src/Vehicle.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Vehicle {
+    private int vehicleId;
+    private String make;
+    private String model;
+    private int year;
+
+    private static ArrayList<Integer> currentVehicleIds = new ArrayList<>();
+
+    public Vehicle(int vehicleId, String make, String model, int year) {
+        if (isUniqueId(vehicleId)) {
+            this.vehicleId = vehicleId;
+            currentVehicleIds.add(vehicleId);
+        } else {
+            throw new IllegalArgumentException("Duplicate vehicleId, please input a number other than " + vehicleId);
+        }
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+	public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    
+    private static boolean isUniqueId(int vehicleId) {
+        return !currentVehicleIds.contains(vehicleId);
+        // true - doesn't exist yet, false, exists
+    }  
+}
