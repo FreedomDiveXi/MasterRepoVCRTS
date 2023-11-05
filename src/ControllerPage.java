590cd71a60cf122a11ecf61a643fe0ba96d315db
Lawrence Lai <144949740+FreedomDiveXi@users.noreply.github.com>
2023-11-05 16:00:08 -0500

    Update ControllerPage.java

--- src/ControllerPage.java
+++ src/ControllerPage.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.time.*;
 
-public class ControllerPage {
-
+public class ControllerPage extends JFrame{
+	private static final int FRAME_WIDTH = 600;
+	private static final int FRAME_HEIGHT = 500;
 }
