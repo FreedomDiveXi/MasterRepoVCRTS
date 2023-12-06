0308bad45a9cdae2997371d7b4d1dda0b19adbad
Lawrence Lai <144949740+FreedomDiveXi@users.noreply.github.com>
2023-12-05 22:55:44 -0500

    Update jobCreationGui.java

--- src/jobCreationGui.java
+++ src/jobCreationGui.java
@@ -1,26 +1,38 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
 public class jobCreationGui {
-    private static final int FRAME_WIDTH = 600;
+    private static final int FRAME_WIDTH = 700;
     private static final int FRAME_HEIGHT = 500;
     JFrame SubmitJob = new JFrame();
     JPanel panel = new JPanel();
     JLabel question1 = new JLabel("Client ID");
-    JTextField clientID = new JTextField(50);
+    JTextField clientID = new JTextField(25);
     JLabel question2 = new JLabel("Job ID");
-    JTextField jobID = new JTextField(50);
-    JLabel question3 = new JLabel("Approximately how long will this job take, please just type in the total amount of hours?");
-    JTextField jobDuration = new JTextField(50);
-    JLabel question4 = new JLabel("If needed what is this job's deadline, please type in this format year-month-day");
-    JTextField jobDeadline = new JTextField(50);
+    JTextField jobID = new JTextField(25);
+    JLabel question3 = new JLabel("<html><div style = 'text-align:center;'>" + 
+    								"Approximately how long will this job take, " +
+    								"please type a number representing the amount of hours." + 
+    								"</div></html>");
+    JTextField jobDuration = new JTextField(25);
+    JLabel question4 = new JLabel("<html><div style = 'text-align:center;'>" + 
+    								"If needed what is this job's deadline, " +
+    								"please type in this format year-month-day" + 
+    								"</div></html>");
+    JTextField jobDeadline = new JTextField(25);
     JButton submitJob = new JButton("Submit");
     ClientConnection clientConnection;
+    JLabel temp1 = new JLabel("");
+    
     public jobCreationGui(ClientConnection connection){
         listenForRequests();
         clientConnection = connection;
+        question1.setHorizontalAlignment(JLabel.CENTER);
+    	question2.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(5, 2, 5, 65));
+        
         panel.add(question1);
         panel.add(clientID);
         panel.add(question2);
@@ -29,6 +41,7 @@ public class jobCreationGui {
         panel.add(jobDuration);
         panel.add(question4);
         panel.add(jobDeadline);
+        panel.add(temp1);
         panel.add(submitJob);
         SubmitJob.add(panel);
         SubmitJob.setSize(FRAME_WIDTH, FRAME_HEIGHT);
